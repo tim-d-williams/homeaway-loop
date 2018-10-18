d4c1f3136f080283c984d69ce7c91ca3375ceb24
Tim Williams <timwilliams.tx@gmail.com>
2018-10-18 11:55:31 -0500

    Add homeawayLoop function

---
+++ src/part1.js
@@ -0,0 +1,14 @@
+homeawayLoop = function() {
+  let i = 100
+  while (i <= 200) {
+    if ((i%3 === 0) && (i%4 === 0)) {
+      console.log("HomeAway");
+    } else if (i%3 === 0) {
+      console.log("Home");
+    } else if (i%4 === 0) {
+      console.log("Away");
+    } else {
+      console.log(i);
+  } i++;
+}
+}
